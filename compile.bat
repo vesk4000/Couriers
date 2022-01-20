f593c46e33d5282061932ee0649d3e164cb7c424
vesk4000 <veskmail@gmail.com>
2022-01-20 07:40:32 +0200

    Added update and delete procedures and fixed queries. Collective commit from Vesk, GreenUncle and IcyDenev

--- compile.bat
+++ compile.bat
@@ -1,25 +1,58 @@
 @echo off
+setlocal
+pushd %~dp0\sql-compiler
 
-echo -- Successfully compiled > couriers.sql
+:: Call the argument-counting subroutine with all arguments received,
+:: without interfering with the ability to reference the arguments
+:: with %1, ... later.
+call :count_args %*
 
-::CALL :NORMALIZEPATH "..\..\..\foo\bar.txt"
-::SET BLAH=%RETVAL%
+:: Print the result.
+::echo %ReturnValue% argument(s) received
 
-::ECHO "%BLAH%"
+set /A a = %ReturnValue%
+if %a% == 0 (start "" sql-compiler.exe "couriers.sql" "examples.sql" "compile.bat" "AllFiles")
+if %a% == 1 (start "" sql-compiler.exe %1 "examples.sql" "compile.bat" "AllFiles")
+if %a% == 2 (start "" sql-compiler.exe %1 %2 "compile.bat" "AllFiles")
+if %a% == 3 (start "" sql-compiler.exe %1 %2 %3 "AllFiles")
+if %a% == 4 (start "" sql-compiler.exe %1 %2 %3 %4)
 
-::for %%i in (
-	::\src\schemas\clients-table.sql
-	::\src\schemas\couriers-table.sql
-::) do (
-	CALL :NORMALIZEPATH \src\schemas\clients-table.sql
-	SET BLAH=%RETVAL%
-	echo %BLAH%
-	::type %~f1 >> couriers.sql
-::)
+:: Exit the batch file.
+exit /b
 
-:: ========== FUNCTIONS ==========
-EXIT /B
+goto AllFiles
 
-:NORMALIZEPATH
-  SET RETVAL=%~f1
-  EXIT /B
+schemas/couriers-database.sql
+schemas/addresses-table.sql
+schemas/clients-table.sql
+schemas/couriers-table.sql
+schemas/dispatchers-table.sql
+schemas/recipients-table.sql
+schemas/types-of-service-table.sql
+schemas/orders-table.sql
+
+data/addresses-data.sql
+data/clients-data.sql
+data/couriers-data.sql
+data/dispatchers-data.sql
+data/recipients-data.sql
+data/types-of-service-data.sql
+data/orders-data.sql
+
+private/*.sql
+**/*.sql
+
+:AllFiles
+
+:: Subroutine that counts the arguments given.
+:: Returns the count in %ReturnValue%
+:count_args
+  set /a ReturnValue = 0
+  :count_args_for
+
+    if %1.==. goto :eof
+
+    set /a ReturnValue += 1
+
+    shift
+  goto count_args_for
---
+++ sql-compiler/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using GlobExpressions;
+
+class Program {
+	static void Main(string[] args) {
+		Directory.SetCurrentDirectory("../");
+
+		string output = "couriers.sql";
+		string exampleOutput = "examples.sql";
+		string patternListSource = "compile.bat";
+		string patternListSet = "AllFiles";
+
+		if(args.Length > 0)
+			output = args[0];
+		if(args.Length > 1)
+			exampleOutput = args[1];
+		if(args.Length > 2)
+			patternListSource = args[2];
+		if(args.Length > 3)
+			patternListSet = args[3];
+
+		string patternsSourceText = File.ReadAllText(patternListSource);
+		int from = patternsSourceText.IndexOf("goto " + patternListSet);
+		int to = patternsSourceText.IndexOf(":" + patternListSet);
+		if(from == -1 || to == -1 || from > to) {
+			Console.WriteLine("Error: Patterns Set doesn't exist!");
+			return;
+		}
+
+		Queue<string> patterns = new Queue<string>(
+			patternsSourceText[from..to]
+			.Split(
+				new string[] { "\r\n", "\r", "\n" },
+				StringSplitOptions.None
+			)
+		);
+		patterns.Dequeue(); // Otherwise "goto patternListSet" is included in patterns
+
+		List<string> includedFiles = new List<string>();
+
+		string outputText = "";
+		string exampleText = "";
+
+		foreach(string pattern in patterns) {
+			foreach(string file in
+				Directory.GetFiles(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories)
+			) {
+				if((!includedFiles.Contains(file)) && Glob.IsMatch(file, "**/source/" + pattern)) {
+					includedFiles.Add(file);
+					outputText += File.ReadAllText(file) + Environment.NewLine;
+					if(getTextBetweenStrings(File.ReadAllText(file), "/* Example", "*/") != "")
+					exampleText += 
+					"-- " + Path.GetRelativePath(Directory.GetCurrentDirectory(), file)
+					+ Environment.NewLine +
+					getTextBetweenStrings(File.ReadAllText(file), "/* Example", "*/")
+					.TrimStart(Environment.NewLine.ToCharArray()) + Environment.NewLine;
+					//Console.WriteLine(file);
+				}	
+			}
+		}
+
+		File.WriteAllText(output, outputText);
+		if(exampleOutput != "")
+			File.WriteAllText(exampleOutput, exampleText);
+
+	}
+
+	static string getTextBetweenStrings(string text, string from, string to) {
+		int fromIndex = text.IndexOf(from);
+		int toIndex = text.IndexOf(to);
+		if(fromIndex == -1 || toIndex == -1)
+			return "";
+		if(fromIndex > toIndex) {
+			int temp = fromIndex;
+			fromIndex = toIndex;
+			toIndex = temp;
+		}
+
+		return (text[fromIndex..toIndex]).Substring(from.Length);
+	}
+}
